edef5c8e97200db14ff855e20269ae4f4006b8d3
Jonathan Elmgren <jonathan@kodtre.se>
2021-05-08 00:50:25 +0200

    managed to get a satisfactory functionality with register/login

--- src/view/signinview/RegisterView.tsx
+++ src/view/signinview/RegisterView.tsx
@@ -1,10 +1,25 @@
+import {useState} from 'react'
+import { useHistory } from 'react-router-dom'
+import RoutingPath from '../../routes/RoutingPath'
+
 export const RegisterView = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const history = useHistory()
+    
+    const registerUser = (username: string, password: string) => {
+        localStorage.setItem('username', username)
+        localStorage.setItem('password', password)
+        history.push(RoutingPath.signInView)
+        alert(`Thank you for registering ${username}`)
+    }
+
     return (
         <div>
             <h1>RegisterView</h1>
-            <input placeholder="username"/> <br />
-            <input placeholder="password"/> <br />
-            <button>submit</button>
+            <input placeholder="username" onChange={(event) => setUsername(event.target.value)} /> <br />
+            <input placeholder="password" onChange={(event) => setPassword(event.target.value)} /> <br />
+            <button onClick={() => registerUser(username,password)}>submit</button>
         </div>
     )
 }
--- src/view/signinview/SignInView.tsx
+++ src/view/signinview/SignInView.tsx
@@ -5,17 +5,27 @@ import RoutingPath from '../../routes/RoutingPath'
 
 export const SignInView = () => {
 	const history = useHistory()
-	const [username, setUsername] = useState('Arasto')
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 	const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
 
-	const signIn = async (event: React.MouseEvent<HTMLElement>) => {
+/* 	const signIn = async (event: React.MouseEvent<HTMLElement>) => {
 		event.preventDefault()
 		try {
-			/* localStorage.setItem(LocalStorage.authenticationToken, data.token) */
+			localStorage.setItem(LocalStorage.authenticationToken, data.token)
 			setAuthenticatedUser(username)
 			history.push(RoutingPath.homeView)
 		} catch (error) {
 			console.log(error)
+		}
+	} */
+
+	const signIn = (usernames: string, passwords: string) => {
+		if(localStorage.getItem("username") === usernames && localStorage.getItem("password") === passwords){
+			setAuthenticatedUser(username)
+			history.push(RoutingPath.homeView)
+		} else {
+			alert('Wrong username or password')
 		}
 	}
 
@@ -24,7 +34,8 @@ export const SignInView = () => {
 			<h1>Login {authenticatedUser}</h1>
 			<form>
 				<input placeholder="username" onChange={(event) => setUsername(event.target.value)} /> <br />
-				<button onClick={(event) => signIn(event)}>Sign in</button>
+				<input placeholder="password" onChange={(event) => setPassword(event.target.value)} /> <br />
+				<button onClick={() => signIn(username,password)}>Sign in</button>
 				<button onClick={() => history.push(RoutingPath.registerView)}>Register</button>
 			</form>
 		</div>
