a32699d5db7fa59c8c884fdcb5864c4a5dcb990c
Jonathan Elmgren <jonathan@kodtre.se>
2021-05-08 17:43:51 +0200

    added useeffect hook to check if user is authenticated

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,27 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import RoutingPath from '../routes/RoutingPath'
+import {useContext, useEffect} from 'react'
 import { HomeView } from '../view/navigationview/HomeView'
 import { RecipeView } from '../view/navigationview/RecipeView'
 import { SignInView } from '../view/signinview/SignInView'
 import { RegisterView } from '../view/signinview/RegisterView'
 import { CreateRecipe } from '../view/profileview/CreateRecipe'
+import {UserContext} from '../shared/providers/UserProvider'
 
 export const Routes = (props: { children?: React.ReactChild }) => {
+	const [, setAuthenticatedUser] = useContext(UserContext)
+
+	const checkIfIfUserIsAuthenticated = () => {
+		const getLocalStorage = localStorage.getItem('username');
+		if (getLocalStorage) {
+			setAuthenticatedUser(getLocalStorage);
+		}
+	};
+
+	useEffect(() => {
+		checkIfIfUserIsAuthenticated();
+	});
+
 	return (
 		<BrowserRouter>
 			{props.children}
