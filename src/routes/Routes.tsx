8f387349388abf7d6c1e29a7217d088ed80b2857
Jonathan Elmgren <jonathan@kodtre.se>
2021-05-12 16:55:22 +0200

    fixfix

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,7 +3,6 @@ import RoutingPath from '../routes/RoutingPath'
 import { HomeView } from '../view/navigationview/HomeView'
 import { RecipeView } from '../view/navigationview/RecipeView'
 import { SignInView } from '../view/signinview/SignInView'
-import { RegisterView } from '../view/signinview/RegisterView'
 import { CreateRecipe } from '../view/profileview/CreateRecipe'
 
 export const Routes = (props: { children?: React.ReactChild }) => {
@@ -14,7 +13,6 @@ export const Routes = (props: { children?: React.ReactChild }) => {
 				<Route exact path={RoutingPath.recipeView} component={RecipeView} />
 				<Route exact path={RoutingPath.signInView} component={SignInView} />
 				<Route exact path={RoutingPath.createRecipeView} component={CreateRecipe} />
-				<Route exact path={RoutingPath.registerView} component={RegisterView} />
 				<Route component={HomeView} />
 			</Switch>
 		</BrowserRouter>
