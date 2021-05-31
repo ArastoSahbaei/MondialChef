03713a9333dabb4bab7b5eef8b4d25f58770534b
arasto <Arasto.Sahbaei@gmail.com>
2021-05-31 17:23:20 +0200

    yes

--- src/view/authenticatedviews/profileview/ProfileView.tsx
+++ src/view/authenticatedviews/profileview/ProfileView.tsx
@@ -2,6 +2,7 @@ export const ProfileView = () => {
 	return (
 		<div>
 			<h1>Info about user etc</h1>
+			<h1>Upload new avatar</h1>
 		</div>
 	)
 }
--- src/view/navigationview/HomeView.tsx
+++ src/view/navigationview/HomeView.tsx
@@ -1,7 +1,44 @@
+
+import Axios from 'axios'
+import { useState } from 'react'
+
 export const HomeView = () => {
+	const [state, setState] = useState<Blob | any>()
+	const formData = new FormData()
+
+	const config = {
+		headers: {
+			'content-type': 'multipart/form-data'
+		}
+	}
+
+
+	const uploadAvatarLol = async () => {
+		try {
+			await Axios.post('http://localhost:3001/user/updateAvatar', formData, config)
+			alert('it worked Lol')
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	const onFormSubmit = (e: any) => {
+		e.preventDefault()
+		formData.append('files', state.file)
+		uploadAvatarLol()
+	}
+
+	const onChange = (e: any) => {
+		setState({ file: e.target.files[0] })
+	}
+
 	return (
 		<div>
-			<h1>HomeView</h1>
+			<form onSubmit={(e) => onFormSubmit(e)}>
+				<h1>File Upload</h1>
+				<input type="file" name="lmao" onChange={(e) => onChange(e)} />
+				<button type="submit">Upload</button>
+			</form>
 		</div>
 	)
 }
